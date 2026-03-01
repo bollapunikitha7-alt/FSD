f6ee17a2b78e4e38f77a95a94f6442d3df347105
BOLLAPU NIKHITHA <bollapunikitha7@gmail.com>
2026-03-02 00:10:09 +0530

    Add files via upload

---
+++ javascript/learning hub/Landing1/Landing.js
@@ -0,0 +1,13 @@
+import "./Landing1.css";
+function Landing({name,age}) {
+  return (
+    <>
+    <p>
+      {name}
+      {age}
+    </p>
+      <h1>Welcome to our LearningHub</h1>
+    </>
+  );
+}
+export default Landing;
